05a548973ad8fad944b09cb5e5fc8f5de3dba7da
Cavaco <nonuuxx@hotmail.com>
2016-05-27 17:04:16 +0100

    First commit, faz as funçoes login/out/register e as funçoes do admin(estou a acabar uma delas)

---
+++ src/Expo/Department.java
@@ -0,0 +1,9 @@
+package Expo;
+
+/**
+ * Created by Cavaco on 27/05/2016 at 17:03 in nunokingler's pc.
+ * File originaly created to fulfill project #ExpoFCT in pacage Expo.
+ * 1996-Present no rights reserved
+ **/
+public interface Department {
+}
